0cb778257b4398f507c7f998c60a7ad1ce9a33a9
Ubuntu <azureuser@filterme.filterme.g2.internal.cloudapp.net>
2015-09-26 21:19:55 +0000

    first test

--- flasktest/flask.py
+++ flasktest/flask.py
@@ -1,4 +1,7 @@
 import flask from Flask
-import render_templete from flask
+import render_template from flask
 
-
+app = Flask(__name__)
+@app.route('/')
+def hello():
+	return render_template("index.html")
